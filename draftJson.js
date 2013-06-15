f591dc0f23451ffdd31f2335f79deeb79dc44462
Philip <ficklephil@hotmail.com>
2013-06-15 07:49:58 +0100

    Initial Commit of Then Home Spike

---
+++ draftJson.js
@@ -0,0 +1,16 @@
+{
+	"request":{
+		"width": 400,
+		"height": 800,
+		"LatBndNWPoint": {Lat:54.02323,Lng:54.0343},
+		"LatBndSEPoint": {Lat:54.02323,Lng:54.0343}
+		}
+	"response":{
+		"item":{
+			"dotPoint":{x:34,y:23},
+			"boxPoint":{x:24,y:53},
+			"price":"£232",
+			"beds":"3"
+		}
+	}	
+}
